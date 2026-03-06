fd574e71220504be227511259df560c0ef10ec7e
sKiDa <skidaggabatee@gmail.com>
2026-03-05 21:50:55 -0500

    upgrade

--- server.js
+++ server.js
@@ -66,8 +66,7 @@ if (savedTokens) {
 userInfo = loadUserInfoFromDisk();
 
 const DRIVE_SCOPES = [
-  'https://www.googleapis.com/auth/drive.file',
-  'https://www.googleapis.com/auth/drive.readonly',
+  'https://www.googleapis.com/auth/drive',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email'
 ];
